a6c65f2c86078c5bcdf2cc26156cc2557c4718d8
yxp <3188934392@qq.com>
2022-08-21 12:44:53 +0800

    fix

--- app/upf/internal/pkg/user/user.go
+++ app/upf/internal/pkg/user/user.go
@@ -3,6 +3,7 @@ package user
 import (
 	"sync"
 	"upf/internal/pkg/rule"
+	"upf/internal/pkg/stat"
 )
 
 type User struct {
@@ -11,8 +12,10 @@ type User struct {
 	m   sync.Mutex
 	//user info
 	rule.ULRule
+	ULStat stat.Stat
 
 	rule.DLRule
+	DLStat stat.Stat
 }
 
 type Users struct {
